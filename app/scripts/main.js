e4c495dfd35d1536fa21b49c56c419bf8e497672
Jason Robert Fincham <JRFincham@yahoo.com>
2015-02-09 14:44:32 -0500

    bands array added

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,20 @@
-console.log('The Iron Yard Rocks');
+var data = {
+  bands: [
+
+    {band: 'Umphreys Mcgee', location: 'Chicago, IL'},
+    {band: 'Phish', location: 'Burlington, VT' },
+    {band: 'Moe', location: 'Utica, NY'},
+    {band: 'The Heavy Pets', location: 'Fort Lauderdale, FL'},
+    {band: 'The Black Crowes', location: 'Marietta, GA'},
+    {band: 'TAUK', location: 'Oyster Bay, NY'},
+    {band: 'Moon Taxi', location: 'Memphis, TN'},
+    {band: 'Dumpstaphunk', location: 'New Orleans, LA'},
+    {band: 'My Morning Jacket', location: 'Louisville, KY'}
+
+    ]
+
+};
+
+var source = $('#entry-template').html();
+var template = Handlebars.compile(source);
+$('.bands').html(template(data));
